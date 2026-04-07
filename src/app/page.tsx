88d1672c504d25053a25e4def4afbf59f5bb06c8
Jeyson Coronado <84329870+jeyson767@users.noreply.github.com>
2026-04-07 01:35:42 -0500

    Quitarcomponents_noexistentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 // src/app/page.tsx
 import Hero from '../components/Hero'
-import ProjectsSection from '@/components/ProjectsSection'
-import ContactSection from '@/components/ContactSection'
-import Footer from '@/components/Footer'
 
 export default function Home() {
     return (
